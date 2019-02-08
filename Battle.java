80ff1b304a4df5349cc18867fa02480386c0031a
gmguru16 <36320275+gmguru16@users.noreply.github.com>
2019-02-08 11:49:51 +0530

    Update Battle.java

--- Battle.java
+++ Battle.java
@@ -11,8 +11,10 @@ class Main
 	    for(int j=0;j<t;j++)
 	    {
 	        int a[]=new int[3];
-	        List<Integer> green=new ArrayList<>();
-	        List<Integer> blue=new ArrayList<>();
+	        List<Integer> green=new ArrayList<>(a[1]);
+	        List<Integer> blue=new ArrayList<>(a[2]);
+			PriorityQueue<Integer> greenq=new PriorityQueue<>(Collections.reverseOrder()); 
+			PriorityQueue<Integer> blueq=new PriorityQueue<>(Collections.reverseOrder());
 	        for(int i=0;i<3;i++)
 	            a[i]=in.nextInt();
             for(int i=0;i<a[1];i++)
@@ -25,29 +27,15 @@ class Main
             {
                 if(green.get(0)>blue.get(0))
                 {
-                    int x=green.get(0)-blue.get(0);
+                    greenq.add(green.get(0)-blue.get(0));
                     green.remove(0);
                     blue.remove(0);
-                    if(green.contains(x))
-                        green.add(green.indexOf(x),x);
-                    else
-                    {
-                        green.add(x);
-                        Collections.sort(green,Collections.reverseOrder());
-                    }
                 }
                 else if(green.get(0)<blue.get(0))
                 {
-                    int x=blue.get(0)-green.get(0);
+                    blueq.add(blue.get(0)-green.get(0));
                     green.remove(0);
                     blue.remove(0);
-                    if(green.contains(x))
-                        blue.add(blue.indexOf(x),x);
-                    else
-                    {
-                        blue.add(x);
-                        Collections.sort(blue,Collections.reverseOrder());
-                    }
                 }
                 else
                 {
@@ -55,22 +43,50 @@ class Main
                     blue.remove(0);
                 }
             }
-            if(blue.size()==0 && green.size()==0)
+            if(blue.size()!=0)
+            {
+                for(int i=0;i<blue.size();i++)
+                    blueq.add(blue.get(i));
+            }
+            else if(green.size()!=0)
+            {
+                for(int i=0;i<green.size();i++)
+                    greenq.add(green.get(i));
+            }
+            while(greenq.size()!=0 && blueq.size()!=0)
+            {
+                if(greenq.peek()>blueq.peek())
+                    greenq.add(greenq.poll()-blueq.poll());
+                else if(greenq.peek()<blueq.peek())
+                    blueq.add(blueq.poll()-greenq.poll());
+                else
+                {
+                    greenq.poll();
+                    blueq.poll();
+                }
+            }
+            
+            if(blueq.size()==0 && greenq.size()==0)
             	System.out.println("green and blue died");
-	    else if(green.size()!=0 && blue.size()==0)
-	    {
-		    System.out.println("green wins");
-		    for(int i=0;i<green.size();i++)
-		   	System.out.println(green.get(i));
-	    }
-            else if(green.size()==0 && blue.size()!=0)
-	    {
-		    System.out.println("blue wins");
-		    for(int i=0;i<blue.size();i++)
-		   	System.out.println(blue.get(i));
-	    }
+    	    else if(greenq.size()!=0 && blueq.size()==0)
+    	    {
+		        System.out.println("green wins");
+		        Object[] g = greenq.toArray();
+		        Arrays.sort(g,Collections.reverseOrder());
+                for ( int i = 0; i<g.length; i++ ) 
+                    System.out.println(g[i]) ; 
+	        }
+            else if(greenq.size()==0 && blueq.size()!=0)
+	        {
+		        System.out.println("blue wins");
+		        Object[] g = blueq.toArray();
+		        Arrays.sort(g,Collections.reverseOrder());
+		        for ( int i = 0; i<g.length; i++ ) 
+                    System.out.println(g[i]) ; 
+	        }   
             if(j+1<t)
-		System.out.println();
+		        System.out.println();
+		  
 	    }
 	}
 }
